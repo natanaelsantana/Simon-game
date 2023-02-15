4883551321f5ff167193849759bef0bfa0afc4ed
Neitan2m <natanael.santana@outlook.com.br>
2023-02-15 16:47:14 -0300

    Click events added to the buttons

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-userClickedPattern = []
+let userClickedPattern = []
 // selecting colors and pattern
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-gamePattern.push(nextSequence, buttonAnimation())
+gamePattern.push(nextSequence, buttonAnimation, btnSound)
 
-
+// everything start with this function
 function nextSequence() {
-    let randomNumber= Math.floor(Math.random() * 4)
+    let randomNumber = Math.floor(Math.random() * 4)
     let randomChosenColor = buttonColors[randomNumber]
     return (randomChosenColor)
 }
 
-
-    let colorSound = new Audio(`sounds/${nextSequence}.mp3`);
-    colorSound.play
+function btnSound () {
+let colorSound = new Audio(`sounds/${nextSequence()}.mp3`);
+colorSound.play()
+}
 
 
 
 // Making press animation
-function buttonAnimation () {
-    let colorBtn =  document.querySelector("#"+ nextSequence());
+function buttonAnimation() {
+    let colorBtn = document.querySelector("#" + nextSequence());
     colorBtn.classList.add("pressed")
-    
-    setTimeout(function() {
-     colorBtn.classList.remove("pressed");
+
+    setTimeout(function () {
+        colorBtn.classList.remove("pressed");
     }, 100);
- 
- }
 
- let containingElement = document.querySelectorAll("#green", "#red", "#blue", "#yellow");
+}
 
-document.addEventListener("click",  function (event) {
-    
-    if(containingElement.contains(event.target)) {
+
+ // adding click event in the buttons 
+
+let redColor = document.querySelector("#red")
+let blueColor = document.querySelector("#blue")
+let yellowColor = document.querySelector("#yellow")
+let greenColor = document.querySelector("#green")
+
+document.addEventListener("click", function (event) {
+
+    if (redColor.contains(event.target)) {
         let userChosenColour = event.target
-        return console.log(userChosenColour)
-    } else {
-        let userChosenColour = ""
+        userClickedPattern.push(userChosenColour)
 
+    } else if (greenColor.contains(event.target)) {
+        let userChosenColour = event.target
+        userClickedPattern.push(userChosenColour)
+
+    } else if (yellowColor.contains(event.target)) {
+        let userChosenColour = event.target
+        userClickedPattern.push(userChosenColour)
+
+    } else if (blueColor.contains(event.target)) {
+        let userChosenColour = event.target
+        userClickedPattern.push(userChosenColour)
     }
 
 })
 
- // adding click event in the buttons 
 
+// start game at a random click
+
+document.addEventListener("click", nextSequence())
